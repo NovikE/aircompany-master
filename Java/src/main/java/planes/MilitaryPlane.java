345c00bb7f97efff68d511ed2d42a0c0b689108a
Alena Novik <alena_novik1@epam.com>
2020-04-05 20:00:04 +0300

    hw module 4: fixed version 1

---
+++ Java/src/main/java/planes/MilitaryPlane.java
@@ -0,0 +1,36 @@
+package planes;
+
+import models.MilitaryType;
+
+import java.util.Objects;
+
+public class MilitaryPlane extends Plane{
+
+    private MilitaryType type;
+
+    public MilitaryPlane(Plane plane, MilitaryType type) {
+        super(plane);
+        this.type = type;
+    }
+
+    public MilitaryType getType() {
+        return type;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString().replace("}",
+                ", type=" + type +
+                '}');
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return (this == o);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), type);
+    }
+}
---
+++ Java/src/main/java/planes/MyPlane.java
@@ -0,0 +1,7 @@
+package Planes;
+
+public class MyPlane extends Plane {
+    public MyPlane(Plane plane) {
+        super(plane);
+    }
+  }
